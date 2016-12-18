f20e15a557b6f46a2e5e1de5b215642498abfd02
Bradley Cravens <bradleycravens@gmail.com>
2016-12-18 09:33:09 -0500

    Validates username and email uniqueness

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,48 @@
 import express from 'express'
-import validateInput from '../shared/validations/signup'
+import commonValidations from '../shared/validations/signup'
 import bcrypt from 'bcrypt'
+import Promise from 'bluebird'
+import isEmpty from 'lodash/isEmpty'
 
 import User from '../modules/user'
 
 let router = express.Router()
 
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data)
+
+  return Promise.all([
+    User.where({ email: data.email }).fetch().then(user => {
+      if (user) { errors.email = "That email is already in use";}
+    }),
+    User.where({ username: data.username }).fetch().then(user => {
+      if (user) { errors.username = "That username is already in use";}
+    })
+  ]).then(() => {
+      return {
+        errors,
+        isValid: isEmpty(errors)
+      }
+  })
+}
+
 router.post('/', (req, res) => {
-  const { errors, isValid } = validateInput(req.body)
+  validateInput(req.body, commonValidations).then(({ errors, isValid }) => {
+    if(isValid) {
+      const { username, password, timezone, email } = req.body
+      const password_digest = bcrypt.hashSync(password, 10)
 
-  if(isValid) {
-    const { username, password, timezone, email } = req.body
-    const password_digest = bcrypt.hashSync(password, 10)
+      User.forge({
+        username, timezone, email, password_digest
+      }, { hasTimestamps: true}).save()
+        .then(user => res.json({ success: true }))
+        .catch(err => res.status(500).json({ error: err }))
 
-    User.forge({
-      username, timezone, email, password_digest
-    }, { hasTimestamps: true}).save()
-      .then(user => res.json({ success: true }))
-      .catch(err => res.status(500).json({ error: err }))
+    } else {
+      res.status(400).json(errors)
+    }
+  })
 
-  } else {
-    res.status(400).json(errors)
-  }
 })
 
 export default router
