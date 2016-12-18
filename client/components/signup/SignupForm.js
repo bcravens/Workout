02c8edecf9915fd2c4ff405eb88aebe84d38cf4f
Bradley Cravens <bradleycravens@gmail.com>
2016-12-17 19:26:09 -0500

    Added redux and now sends data to backend

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import timezones from '../../data/timezones'
 import map from 'lodash/map'
-import axios from 'axios'
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -26,7 +25,7 @@ class SignupForm extends React.Component {
 
   onSubmit(e) {
     e.preventDefault()
-    axios.post('/api/users', {user: this.state})
+    this.props.userSignupRequest(this.state)
   }
 
   render() {
@@ -102,6 +101,10 @@ class SignupForm extends React.Component {
             </form>
     )
   }
+}
+
+SignupForm.propTypes = {
+  userSignupRequest:  React.PropTypes.func.isRequired
 }
 
 export default SignupForm
