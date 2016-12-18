dc186eb3d600d9a5ce433a5d119f630472b33048
Bradley Cravens <bradleycravens@gmail.com>
2016-12-17 23:36:52 -0500

    Renders a flash message on login and user can delete them

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './NavBar'
+import FlashMessagesList from './flash/FlashMessagesList'
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <NavBar />
+        <FlashMessagesList />
         {this.props.children}
       </div>
     )
